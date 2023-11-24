9173ed00c457ce1b084c1780651a37c8aabbddd2
Andrew Grigoriev <grigorievfinance@gmail.com>
2023-11-24 16:45:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import uvicorn
+
+from fastapi import FastAPI
+from model import models
+from model.database import engine
+from routers.items import router as items_router
+from routers.users import router as users_router
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+app.include_router(
+    router=items_router,
+    prefix='/items',
+)
+
+app.include_router(
+    router=users_router,
+    prefix='/users',
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
---
+++ routers/users.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+
+from model import schemas
+from controllers.users import get_users
+from model.database import get_db
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
+    return users
