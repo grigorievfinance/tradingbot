056bcb6210a696591c1b480938f446e3f1df5dbc
Andrew Grigoriev <grigorievfinance@gmail.com>
2023-11-27 11:49:38 +0200

    create token

--- models/model.py
+++ models/model.py
@@ -15,6 +15,17 @@ class User(Base):
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
+    tokens = relationship("Token", back_populates="user")
+
+
+class Token(Base):
+    __tablename__ = "tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    access_token = Column(String, unique=True, index=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="tokens")
 
 
 class Item(Base):
